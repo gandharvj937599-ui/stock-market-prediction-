c9fdf1650f31e1aec97c4c2d12e9b865135ebb87
gandharvj937599-ui <gandharvj937599@gmail.com>
2025-10-07 13:20:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  root: '.',
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
 })
